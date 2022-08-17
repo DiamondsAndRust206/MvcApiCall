cfa8c127e504fe302e4a97f1855fa38dd132662a
Timothy RYAN <ryaninlux@gmail.com>
2022-08-16 20:03:30 -0700

    all other files

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcApiCall.Models;
+
+namespace MvcApiCall.Controllers
+{
+  public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var allArticles = Article.GetArticles("EvnironmentVariables.ApiKey");
+            return View(allArticles);
+        }
+    }
+}
